ab187a62a178edc8fa87882aefec695dd181525a
Vadim Litau <vadimlitau@yandex.ru>
2021-09-26 23:13:20 +0600

    Kurlik kurlik i v prodachn

--- vendor/js/index.js
+++ vendor/js/index.js
@@ -1,54 +1,52 @@
-/*Начало - Заполнение полей форм popup*/
-const popup_name = document.querySelector('.profile__info-name'); /*получение элемента с именем профиля*/
-const popup_nameText = popup_name.textContent; /* получение текста имени профиля*/
-const popup_nameId = document.getElementById('popup__idName'); /*получение инпута имени попап */
-/*ниже аналогия имени но с профессией*/
+//Начало - Заполнение полей форм popup
+//получение элемента с именем профиля
+const popup_name = document.querySelector('.profile__info-name');
+//получение текста имени профиля
+const popup_nameText = popup_name.textContent;
+//получение инпута имени попап
+const popup_nameId = document.getElementById('popup__idName');
+//ниже аналогия имени но с профессией
 const popup_profession = document.querySelector('.profile__info-profession');
 let popup_professionText = popup_profession.textContent;
 const popup_professionId = document.getElementById('popup__idProfession');
-/*Конец - Заполнение полей форм popup*/
-/*начало - открытие popup_opened*/
-const popup_button = document.querySelector('.profile__info-button'); /*нашли кнопку открытия popup*/
-const popup = document.querySelector('.popup'); /*нашли popup*/
-popup_button.addEventListener('click', popup_opened); /*слушатель для popup*/
-/*функция обработчик popup*/
-function popup_opened() {
+//Конец - Заполнение полей форм popup
+//начало - открытие popup_opened
+//нашли кнопку открытия popup
+const popup_button = document.querySelector('.profile__info-button');
+//нашли popup
+const popup = document.querySelector('.popup');
+//слушатель для popup
+popup_button.addEventListener('click', function() {
     if (!popup.classList.contains('popup_opened')) {
         popup.classList.add('popup_opened');
         popup_nameId.value = popup_nameText;
         popup_professionId.value = popup_professionText;
     }
-};
-/*Конец - открытие popup_opened*/
-/*___________________________*/
-/*начало - закрытие popup_opened*/
-const popup_closetButton = document.querySelector('.popup__cross'); /*нашли кнопку закрытия popup*/
-popup_closetButton.addEventListener('click', popup_closet); /*слушатель для popup*/
-/*функция обработчик закрытия popup*/
-function popup_closet() {
+});
+//Конец - открытие popup_opened
+//начало - закрытие popup_opened
+//нашли кнопку закрытия popup
+const popup_closetButton = document.querySelector('.popup__cross');
+//слушатель для popup
+popup_closetButton.addEventListener('click', function() {
     if (popup.classList.contains('popup_opened')) {
         popup.classList.remove('popup_opened');
 
     }
-};
-/*Конец - закрытие popup_opened*/
-/*___________________________*/
-
-/*Начало - ввод в попап и сохранение на странице*/
+});
+//Конец - закрытие popup_opened
+//Начало - ввод в попап и сохранение на странице
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('#popup__idName');
 const jobInput = document.querySelector('#popup__idProfession');
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
+formElement.addEventListener('submit', function(evt) {
     popup_name.textContent = nameInput.value;
     popup_profession.textContent = jobInput.value;
     popup.classList.remove('popup_opened');
-}
-formElement.addEventListener('submit', formSubmitHandler);
-/*Конец - ввод в попап и сохранение на странице*/
-/*___________________________*/
-/*Начало - добавление элемента галлереи*/
+    evt.preventDefault();
+});
+//Конец - ввод в попап и сохранение на странице
+//Начало - добавление элемента галлереи
 const initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -74,131 +72,139 @@ const initialCards = [{
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-
-const gallery = document.querySelector('.gallery-element'); /*получение элемента родителя галереи*/
-const galleryElement = document.querySelector('#photo-gallery__element').content; /*получение контента template */
-const popupPhoto = document.querySelector('.popup-photo'); /*Получение элемента popupPhoto */
-let popupPhotoImage = document.querySelector('.popup-photo__photo');
-let popupPhotoName = document.querySelector('.popup-photo__name');
-
-
-/*добавление карточек галереи по умолчанию*/
+//Начало - добавление карточек галереи по умолчанию
+//получение элемента родителя галереи
+const gallery = document.querySelector('.gallery-element');
+// добавление карточки галереи из объекта по умолчанию
 initialCards.forEach(function(item) {
-    let newGalleryElement = galleryElement.querySelector('.gallery-element__item').cloneNode(true); /*копирование содержимого template*/
-    let galleryImage = newGalleryElement.querySelector('.gallery-element__photo'); /*получение элемента с изображением*/
-    let galleryText = newGalleryElement.querySelector('.gallery-element__caption-name'); /*получение элемента с текстом */
-    galleryImage.src = item.link; /*замена изображения из объекта по умолчанию */
-    galleryText.textContent = item.name; /*замена текста из объекта по умолчанию*/
-    gallery.append(newGalleryElement); /*добавление карточки галереи */
-    /*Начало - Анимация Лайка*/
+    //получение контента template
+    const galleryElement = document.querySelector('#photo-gallery__element').content;
+    //получение копии template элемента
+    const newGalleryElement = galleryElement.querySelector('.gallery-element__item').cloneNode(true);
+    //получение изображение из копии
+    const galleryImage = newGalleryElement.querySelector('.gallery-element__photo');
+    //получение текста из копии
+    const galleryText = newGalleryElement.querySelector('.gallery-element__caption-name');
+    //замена изображения из объекта по умолчанию
+    galleryImage.src = item.link;
+    //замена текста из объекта по умолчанию
+    galleryText.textContent = item.name;
+    //Начало - Анимация Лайка
+    //получение элемента лайка из копии
     const galleryLike = newGalleryElement.querySelector('.gallery-element__caption-like');
-
-    function editLike() {
+    //реакиця на клик по лайку и замена стиля
+    galleryLike.addEventListener('click', function() {
         galleryLike.classList.toggle('gallery-element__caption-like_active');
-    }
-    galleryLike.addEventListener('click', editLike);
-    /*Конец - Анимация Лайка*/
-
-    /*Начало - Удаление карточки*/
+    });
+    //Конец - Анимация Лайка
+    //Начало - Удаление карточки
+    //получение элемента удаления карточки из копии
     const galleryDeletCard = newGalleryElement.querySelector('.gallery-element__deletCard');
-
-    function deletCard() {
+    //реакция на клик по кнопке удаления
+    galleryDeletCard.addEventListener('click', function() {
         newGalleryElement.remove();
-    };
-    galleryDeletCard.addEventListener('click', deletCard);
-    /*Конец - Удаление карточки*/
-    /* */
-    function showPhoto() {
+    });
+    //Конец - Удаление карточки
+    //Начало - popup показать фото
+    //получение родителя для показа фотографии
+    const popupPhoto = document.querySelector('.popup-photo');
+    //получение изображения для показа фотографии
+    const popupPhotoImage = document.querySelector('.popup-photo__photo');
+    //получение текста для показа изображения
+    const popupPhotoName = document.querySelector('.popup-photo__name');
+    //реакция на нажатие на изображение и открытие попап
+    galleryImage.addEventListener('click', function() {
         popupPhoto.classList.add('popup-photo_opened');
         popupPhotoImage.src = item.link;
         popupPhotoName.textContent = item.name;
-    }
-    galleryImage.addEventListener('click', showPhoto);
-
-    function closePhoto() {
+    });
+    //получение элемента для закрытие попап показа изображения
+    const popupPhotoClose = document.querySelector('.popup-photo__cross');
+    popupPhotoClose.addEventListener('click', function() {
         popupPhoto.classList.remove('popup-photo_opened');
-    }
-    popupPhoto.addEventListener('click', closePhoto);
-    /* */
+    });
+    gallery.append(newGalleryElement);
 });
-/*Конец - добавление элемента галлереи */
-/*___________________________*/
-/*Начало - Форма добавление карточки*/
+//Конец - добавление карточек галереи по умолчанию
 
-
-const popup_templateAddCardOpen = document.querySelector('#popup').content; /*template элемент popup */
-const popup_CloneAddCardOpen = popup_templateAddCardOpen.querySelector('.popup').cloneNode(true);
-const popup_buttonAddCard = document.querySelector('.profile__button'); /*нашли кнопку открытия popup*/
-const popupAddCardClos = popup_CloneAddCardOpen.querySelector('.popup__cross'); /*кнопка закрытия popup*/
-popup_buttonAddCard.addEventListener('click', popup_addCardOpen);
-popupAddCardClos.addEventListener('click', popupCardClose);
-let popup_addCardTitle = popup_CloneAddCardOpen.querySelector('.popup__form-title');
-const popContent = document.querySelector('.content');
-
-function popup_addCardOpen() {
-    if (!popup_CloneAddCardOpen.classList.contains('popup_opened')) {
-        popContent.append(popup_CloneAddCardOpen);
-        popup_CloneAddCardOpen.classList.add('popup_opened');
-        popup_addCardTitle.textContent = 'Новое место';
-        popup_CloneAddCardOpen.querySelector('#popup__firstInput').placeholder = 'Название';
-        popup_CloneAddCardOpen.querySelector('#popup__secondInput').placeholder = 'Ссылка на картинку';
-
-    }
-};
-
-function popupCardClose() {
-    if (popup_CloneAddCardOpen.classList.contains('popup_opened')) {
-        popup_CloneAddCardOpen.classList.remove('popup_opened');
-
-    }
-};
-/*Конец - Форма добавление карточки console.log(popupAddCardClos);*/
-/*___________________________*/
-/*Начало - Добавление карточки*/
-const popupAddCardSubmitButton = popup_CloneAddCardOpen.querySelector('.popup__form-button');
-
-function addNewGalleryCard(evt) {
+//добавление карточки и закрытие попап
+//получение кнопки для добавления карточки
+const addNewCard = document.querySelector('.popupAddCard__form-button');
+//получение текста из попап для названия карточки
+const newCardText = document.querySelector('#popupAddCard__firstInput');
+//получение ссылки на изображение из попап для карточки
+const newCardLink = document.querySelector('#popupAddCard__secondInput');
+//добавления карточки пользвателем и закрытие попап
+addNewCard.addEventListener('click', function(evt) {
     evt.preventDefault();
-    let nameNewCard = popup_CloneAddCardOpen.querySelector('#popup__firstInput').value;
-    let linkNewCard = popup_CloneAddCardOpen.querySelector('#popup__secondInput').value;
-    let newGalleryElement = galleryElement.querySelector('.gallery-element__item').cloneNode(true); /*копирование содержимого template*/
-    let galleryImage = newGalleryElement.querySelector('.gallery-element__photo'); /*получение элемента с изображением*/
-    let galleryText = newGalleryElement.querySelector('.gallery-element__caption-name'); /*получение элемента с текстом */
-    galleryImage.src = linkNewCard; /*замена изображения из объекта по умолчанию */
-    galleryText.textContent = nameNewCard; /*замена текста из объекта по умолчанию*/
-    gallery.prepend(newGalleryElement); /*добавление карточки галереи */
-    popup_CloneAddCardOpen.classList.remove('popup_opened');
-    /*Начало - Анимация Лайка*/
+    //получение контента template
+    const galleryElement = document.querySelector('#photo-gallery__element').content;
+    //получение копии template элемента
+    const newGalleryElement = galleryElement.querySelector('.gallery-element__item').cloneNode(true);
+    //получение изображение из копии
+    const galleryImage = newGalleryElement.querySelector('.gallery-element__photo');
+    //получение текста из копии
+    const galleryText = newGalleryElement.querySelector('.gallery-element__caption-name');
+    //замена изображения на введенное пользователем
+    galleryImage.src = newCardLink.value;
+    //замена текста на введенный пользователем
+    galleryText.textContent = newCardText.value;
+    //Начало - Анимация Лайка
+    //Получение лайка в новой карточке
     const galleryLike = newGalleryElement.querySelector('.gallery-element__caption-like');
-
-    function editLike() {
+    //реакция и замена вида лайка при клике
+    galleryLike.addEventListener('click', function() {
         galleryLike.classList.toggle('gallery-element__caption-like_active');
-    }
-    galleryLike.addEventListener('click', editLike);
-    /*Конец - Анимация Лайка*/
-    /*___________________________*/
-    /*Начало - Удаление карточки*/
+    });
+    //Конец - Анимация Лайка
+    //Начало - Удаление карточки
+    //получение элемента корзины новой карточки
     const galleryDeletCard = newGalleryElement.querySelector('.gallery-element__deletCard');
-
-    function deletCard() {
+    //реакция на щелчек по корзине - удаление карточки   
+    galleryDeletCard.addEventListener('click', function() {
         newGalleryElement.remove();
-    };
-    galleryDeletCard.addEventListener('click', deletCard);
-    /*Конец - Удаление карточки*/
-    /* */
-    function showPhoto() {
+    });
+    //Конец - Удаление карточки
+    //Начало - popup показать фото
+    //получение родителя для показа фотографии
+    const popupPhoto = document.querySelector('.popup-photo');
+    //получение изображения для показа фотографии
+    const popupPhotoImage = document.querySelector('.popup-photo__photo');
+    //получение текста для показа изображения
+    const popupPhotoName = document.querySelector('.popup-photo__name');
+    //реакция на нажатие на изображение и открытие попап
+    galleryImage.addEventListener('click', function() {
         popupPhoto.classList.add('popup-photo_opened');
-        popupPhotoImage.src = linkNewCard;
-        popupPhotoName.textContent = nameNewCard;
-    }
-    galleryImage.addEventListener('click', showPhoto);
-
-    function closePhoto() {
+        popupPhotoImage.src = newCardLink.value;
+        popupPhotoName.textContent = newCardText.value;
+    });
+    //получение элемента для закрытие попап показа изображения
+    const popupPhotoClose = document.querySelector('.popup-photo__cross');
+    popupPhotoClose.addEventListener('click', function() {
         popupPhoto.classList.remove('popup-photo_opened');
+    });
+    gallery.append(newGalleryElement);
+    //закрытие попап
+    popupAddCard.classList.remove('popupAddCard_opened');
+    //добавление карточки в начало галереи
+    gallery.prepend(newGalleryElement);
+
+});
+//Конец - добавление элемента галлереи
+//Начало - Форма добавление карточки
+//нашли попап popupAddCard
+const popupAddCard = document.querySelector('.popupAddCard');
+//нашли кнопку открытия popup
+const popup_buttonAddCard = document.querySelector('.profile__button');
+//кнопка закрытия popup
+const popupAddCardClos = document.querySelector('.popupAddCard__cross');
+//слушатель на открытие popupAddCard
+popup_buttonAddCard.addEventListener('click', function() {
+    if (!popupAddCard.classList.contains('popupAddCard_opened')) {
+        popupAddCard.classList.add('popupAddCard_opened');
     }
-    popupPhoto.addEventListener('click', closePhoto);
-    /* */
-};
-popupAddCardSubmitButton.addEventListener('click', addNewGalleryCard);
-/*Конец - Добавление карточки*/
-/*___________________________*/
+});
+//слушатель на закрытие popupAddCard
+popupAddCardClos.addEventListener('click', function() {
+    popupAddCard.classList.remove('popupAddCard_opened');
+});
